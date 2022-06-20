8450c4aa45697d220a8ff55c13c5228c3fdb590b
Rafet Durgut <durgutrafet@gmail.com>
2022-06-20 18:14:46 +0100

    error fix

--- RunAll.py
+++ RunAll.py
@@ -7,7 +7,7 @@ def thread_function(conf):
     time.sleep(1)
     print(conf)
     os.system(f"python ./Run_Experiment.py {' '.join(map(str,conf.values()))}")
-parameters = {"pNo":2500,"Method": ["average","average"], "W": [10,25,50,100], "eps": [0.3, 0.4, 0.5], "alpha": [0.1, 0.5, 0.9],"gama": [0.1 ,0.5, 0.9],"learning_mode":[0,1],"loadFileName":["None"],'reward_func':[0,1]}
+parameters = {"pNo":[2500],"Method": ["average","average"], "W": [10,25,50,100], "eps": [0.3, 0.4, 0.5], "alpha": [0.1, 0.5, 0.9],"gama": [0.1 ,0.5, 0.9],"learning_mode":[0,1],"loadFileName":["None"],'reward_func':[0,1]}
 configurations = [dict(zip(parameters, v)) for v in product(*parameters.values())]
 for c in configurations:
     x = threading.Thread(target=thread_function, args=(c,))
