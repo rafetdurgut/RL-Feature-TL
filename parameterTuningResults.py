e17d657fd542940659f98c6595033e14272a90ba
Rafet Durgut <durgutrafet@gmail.com>
2022-06-21 17:28:15 +0100

    file existing control

--- parameterTuningResults.py
+++ parameterTuningResults.py
@@ -6,6 +6,7 @@ import numpy as np
 import csv
 from itertools import product
 from Problem import *
+from os.path import exists
 def get_best_data(fileName, operator_size):
     datas = []
     with open(fileName) as csv_file:
@@ -33,6 +34,8 @@ def thread_function(conf):
     os.system(f"python ./Run_Experiment.py {' '.join(map(str,conf.values()))}")
 parameters = {"pNo":[2500],"Method": ["average","average"], "W": [10,25,50,100], "eps": [0.3, 0.4, 0.5], "alpha": [0.1, 0.5, 0.9],"gama": [0.1 ,0.5, 0.9],"loadFileName":["None"],'reward_func':[0,1]}
 configurations = [dict(zip(parameters, v)) for v in product(*parameters.values())]
+ss = []
+
 for c in configurations:
     data_RL =[]
     data_RL_mean =[]
@@ -44,29 +47,31 @@ for c in configurations:
     data_CRL_max =[]
     data_CRL_std =[]
     ind = 0
-    ss = []
     pno = 2500
     problem = OneMax(pno)
     learned = False
 
     file_name = f"results/convergence-CLRL-4-{c['Method']}-{c['eps']}-{c['W']}-{c['alpha']}-{c['gama']}-0-{learned}-{c['reward_func']}-{problem.dosyaAdi}.csv"
-    data =get_best_data(file_name, 3)
-    data_RL.append(data)
-    data_RL_mean.append(np.mean(data))
-    data_RL_max.append(np.max(data))
-    data_RL_std.append(np.std(data))
+    file_name2 = f"results/convergence-CLRL-4-{c['Method']}-{c['eps']}-{c['W']}-{c['alpha']}-{c['gama']}-0-{learned}-{c['reward_func']}-{problem.dosyaAdi}.csv"
+    if exists(file_name) and exists(file_name2):
+        data =get_best_data(file_name, 3)
+        data_RL.append(data)
+        data_RL_mean.append(np.mean(data))
+        data_RL_max.append(np.max(data))
+        data_RL_std.append(np.std(data))
 
-    file_name = f"results/convergence-CLRL-4-{c['Method']}-{c['eps']}-{c['W']}-{c['alpha']}-{c['gama']}-1-{learned}-{c['reward_func']}-{problem.dosyaAdi}.csv"
-    data =get_best_data(file_name, 3)
-    data_CRL.append(data)
-    data_CRL_mean.append(np.mean(data))
-    data_CRL_max.append(np.max(data))
-    data_CRL_std.append(np.std(data))
+        file_name = f"results/convergence-CLRL-4-{c['Method']}-{c['eps']}-{c['W']}-{c['alpha']}-{c['gama']}-1-{learned}-{c['reward_func']}-{problem.dosyaAdi}.csv"
+        data =get_best_data(file_name, 3)
+        data_CRL.append(data)
+        data_CRL_mean.append(np.mean(data))
+        data_CRL_max.append(np.max(data))
+        data_CRL_std.append(np.std(data))
 
-    if data_CRL_mean[ind] != data_RL_mean[ind]:
-        w,p = wilcoxon(data_RL[ind],data_CRL[ind])
-        print(p)
-        ss.append(p)
-    else:
-        ss.append(1)
+        if data_CRL_mean[ind] != data_RL_mean[ind]:
+            w,p = wilcoxon(data_RL[ind],data_CRL[ind])
+            print(p)
+            print(c)
+            ss.append(p)
+        else:
+            ss.append(1)
 print(ss)
