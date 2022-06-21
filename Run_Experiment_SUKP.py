2ba700cbdc4613e48786b758fa0475d0acf46fbb
Rafet Durgut <durgutrafet@gmail.com>
2022-06-21 23:17:39 +0100

    RUN SUKP

--- Run_Experiment_SUKP.py
+++ Run_Experiment_SUKP.py
@@ -1,3 +1,5 @@
+
+
 #%% Run Experiment
 # Her bir region'da farkli credit degerleri olacak.
 # Not only cluster center, But also cluster power.
@@ -18,30 +20,20 @@ eps = float(sys.argv[4])
 alpha = float(sys.argv[5])
 gama = float(sys.argv[6])
 learning_mode = int(sys.argv[7])
-# rewardType = "average"
-# W = 25
-# eps = 0.3
-# alpha = 0.3
-# gama = 0.9
-# pNo = 4500
+loadFileName=sys.argv[8]
+reward_func = int(sys.argv[9])
+if loadFileName == "None":
+    loadFileName = None
+
 
 problem= SetUnionKnapsack('Data/SUKP',pNo)
-abc = BinaryABC(problem, pop_size=20, maxFE=40*max(problem.m, problem.n), limit=100)
+abc = BinaryABC(problem, pop_size=20, maxFE=40*max(problem.m, problem.n), limit=50)
 
-# problem= OneMax(pNo)
-# abc = BinaryABC(problem, pop_size=20, maxFE=250*40, limit=100)
-# loadFileName="results/clusters-CLRL-4-extreme-0.5-25-0.5-0.3-0-False-1000.csv"
-loadFileName=None
-# learning_mode = 0
-operator_pool = [ flipABC(),  nABC(), BABC(), ibinABC()]
-# operator_pool = [ , binABC(), ibinABC()]
-parameters = {'operator_size': len(operator_pool), 'reward_type': rewardType, 'W':W, 'alpha':alpha, 'gama':gama, 'eps':eps, 'learning_mode':learning_mode, 'load_file':loadFileName}
+operator_pool = [ flipABC(), nABC(), ibinABC(), NBABC() ]
+parameters = {'operator_size': len(operator_pool), 'reward_type': rewardType, 'W':W, 'alpha':alpha, 'gama':gama, 'eps':eps, 'learning_mode':learning_mode, 'load_file':loadFileName, 'reward_func':reward_func}
 operator_selectors = CLRL(parameters)
 alg_outs = ["convergence"]
-# alg_outs = []
-# aos_outs = ["cluster_history","credit_history","operator_informations"]
-# aos_outs = ["credits","rewards","usage_counter","success_counter","cluster_history","clusters","credit_history","reward_history","operator_informations"]
-aos_outs = ["cluster_history","operator_informations"]
+aos_outs = ["operator_informations","clusters","credits","cluster_update_counts"]
 exp = Experiment(abc,operator_pool,operator_selectors,
 problem,algortihm_outs=alg_outs, aos_outs=aos_outs, runs=30, clear_history=True)
 exp.Run()
