2c626ded2a1c34d6e74648d8b10ef36560ceb651
Rafet Durgut <durgutrafet@gmail.com>
2022-06-28 16:28:31 +0100

    Update RunAllSUKP.py

--- RunAllSUKP.py
+++ RunAllSUKP.py
@@ -7,7 +7,7 @@ def thread_function(conf):
     time.sleep(1)
     print(conf)
     os.system(f"python ./Run_Experiment_SUKP.py {' '.join(map(str,conf.values()))}")
-parameters = {"pNo":np.arange(0,30),"Method": ["extreme"], "W": [25], "eps": [0.3], "alpha": [ 0.9],"gama": [0.5],"learning_mode":[0,1],"loadFileName":["None"],'reward_func':[0,1],"credit_func":[0,1],"load_func":[0]}
+parameters = {"pNo":np.arange(0,30),"Method": ["extreme"], "W": [25], "eps": [0.3], "alpha": [ 0.9],"gama": [0.5],"learning_mode":[0,1],"loadFileName":["None"],'reward_func':[1],"credit_func":[1],"load_func":[0]}
 configurations = [dict(zip(parameters, v)) for v in product(*parameters.values())]
 for c in configurations:
     x = threading.Thread(target=thread_function, args=(c,))
